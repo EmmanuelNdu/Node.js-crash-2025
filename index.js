901846a36a6001d73b622e9262b540a50ffc7fd7
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2025-02-20 12:21:55 +0100

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+import  getPost, {getPostLength}  from "./postController.js";
+
+console.log(`Post Lenght: ${getPostLength()}`);
+
+console.log(getPost());
+// const {generateRandomNumber, celcuisToFahrenheit} = require('./utils');
+// console.log(`Random Number: ${generateRandomNumber()}`);
+// console.log(`Celcuis to Farenheit: ${celcuisToFahrenheit(0)}`);
