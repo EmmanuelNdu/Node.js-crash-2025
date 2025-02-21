7ebe40ae1bf8ce97d55f393230cfa2ccd9480b56
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2025-02-22 00:07:54 +0100

    server

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import http from 'http';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World!');
-    res.end();
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+
+    res.writeHead(500, {'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Server Error'}));
 });
 
 server.listen(PORT ,() => {
-    console.log(`Server running on Port ${PORT}`);
+    console.log(`Server running on Port ${PORT}`); 
 });
